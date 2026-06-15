fd96af19a96ebba6873f2d9916fb8a5865992bae
pik <123pik1@proton.me>
2026-06-15 14:07:16 +0200

    works without braces

--- calc.nix
+++ calc.nix
@@ -24,17 +24,19 @@ let
         if op == "^" then 3
         else if op == "*" || op == "/" then 2
         else if op == "+" || op == "-" then 1
+        else if op == "(" || op == ")" then -1
         else 0;
 
     calculateONP = token_list:
-        builtins.foldl' (stack: token:
-        if builtins.isInt token || builtins.isFloat token
+    let 
+        finalStack = builtins.foldl' (stack: token:
+        if getPriority token == 0
         then
-            [ token ] ++ stack
+            [ (builtins.fromJSON token) ] ++ stack
         else
             let
-                a = builtins.head stack;
-                b = builtins.head (builtins.tail stack);
+                b = builtins.head stack;
+                a = builtins.head (builtins.tail stack);
 
                 rest_of_stack = builtins.tail (builtins.tail stack);
 
@@ -48,17 +50,66 @@ let
             in
             [ result ] ++ rest_of_stack
         ) [] token_list;
+    in
+        builtins.head finalStack;
 
-    parseToONP = token_list: input_string: operators:
-        0;
+    divideTokens = string:
+        let 
+            cleared_text = builtins.replaceStrings [" "] [""] string;
+          splitted = builtins.split "(\\+|-|\\*|/|\\^|\\(|\\))" cleared_text;
+        in
+            builtins.filter (x: x!= "") (
+                builtins.map (x: if builtins.isList x 
+                then builtins.head x
+                else x)
+                splitted
+            );
 
+    manageOutputAndOperators = newToken: operators: currentOutput:
+    if getPriority newToken == 0 then
+        {
+            output =  currentOutput ++ [ newToken ];
+            operators = operators;
+        }
+    else
+    if operators == [] then
+        {
+            output = currentOutput;
+            operators = [newToken];
+        }
+    else
+        let
+            topOperator = builtins.head operators;
+            restOperators = builtins.tail operators;
+        in
+        if getPriority newToken > getPriority topOperator then
+            {
+                output = currentOutput;
+                operators =  [newToken] ++ operators;
+            }
+        else
+        let 
+            newOutput = currentOutput ++ [ topOperator ];
+        in
+        manageOutputAndOperators newToken restOperators newOutput;
+
+
+    parseToONP = token_list: operators: output:
+        if token_list == []
+            then output ++ operators
+        else 
+            let
+                headToken = builtins.head token_list;
+                tailTokens = builtins.tail token_list;
+
+                managed = manageOutputAndOperators headToken operators output;
+            in
+            parseToONP tailTokens managed.operators managed.output
+        ;
+    
+    calc = string:
+        calculateONP (parseToONP (divideTokens string) [] []);
+    
 
 in 
-{
-    add = addition;
-    sub = substraction;
-    mul = multiplication;
-    div = division;
-    pow = power;
-    onp = calculateONP;
-}
+    calc
