4bb3fd80de6d4a150a30c1df7bddf1b09b3f5a63
pik <123pik1@proton.me>
2026-06-15 12:45:58 +0200

    added onp

--- calc.nix
+++ calc.nix
@@ -20,6 +20,39 @@ let
     power = base: pow:
         powerAccumulation base pow 1;
 
+    getPriority = op:
+        if op == "^" then 3
+        else if op == "*" || op == "/" then 2
+        else if op == "+" || op == "-" then 1
+        else 0;
+
+    calculateONP = token_list:
+        builtins.foldl' (stack: token:
+        if builtins.isInt token || builtins.isFloat token
+        then
+            [ token ] ++ stack
+        else
+            let
+                a = builtins.head stack;
+                b = builtins.head (builtins.tail stack);
+
+                rest_of_stack = builtins.tail (builtins.tail stack);
+
+                result = if token == "+" then (addition a b)
+                    else if token == "-" then (substraction a b)
+                    else if token == "/" then (division a b)
+                    else if token == "*" then (multiplication a b)
+                    else if token == "^" then (power a b)
+                    else 0;
+
+            in
+            [ result ] ++ rest_of_stack
+        ) [] token_list;
+
+    parseToONP = token_list: input_string: operators:
+        0;
+
+
 in 
 {
     add = addition;
@@ -27,4 +60,5 @@ in
     mul = multiplication;
     div = division;
     pow = power;
+    onp = calculateONP;
 }
